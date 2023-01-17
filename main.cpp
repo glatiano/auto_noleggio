b5e6cfe095dc6ec62db5cb500be75d6e4c9d96ec
glatiano <122810024+glatiano@users.noreply.github.com>
2023-01-17 20:07:38 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,10 +4,6 @@
 using namespace std;
 struct automobile{
     string categoria;
-    string marca;
-    string modello;
-    string colore;
-    string lun, mar, mer, gio, ven, sab;
     };
 int righe=0;
 void contarighe(){
@@ -18,33 +14,74 @@ void contarighe(){
         getline(fin, app);
         righe++;
         }
-    cout<<righe;
     fin.close();
 }
 void richiesta(automobile autom){
+    contarighe();
+    int s=0, r=0, app[10], n=1, y=0;
     string mat[righe][11];
-    int s;
-    string w, app, str;
+    string w, str[10], ap;
     char x=',';
     ifstream fin("auto.csv");
     if (!fin)
         cout<<"Errore di apertura del file";
-    contarighe();
     while (!fin.eof()){
-        for (int c=0; c<11; c++)
+        for (int r=0; r<righe; r++)
         {
-            for (int r=0; r<righe; r++)
+            for (int c=0; c<11; c++)
             {
-                fin>>w;
-                w.erase(remove(w.begin(),w.end(),x),w.end());
-                if (w!=",")
-                    mat[c][r]=w;
-                cout<<mat[c][r]<<" ";
+                getline(fin,mat[r][c],',');
+                cout<<mat[r][c]<<" ";
             }
             cout<<endl;
         }
     }
     fin.close();
+    ifstream sin("auto.csv");
+    if (!sin)
+        cout<<"Errore di apertura del file";
+    cout<<"Inserire la categoria dell'auto : ";
+    cin>>autom.categoria;
+    while (n!=0){
+        cout<<"Inserire i giorni in cui serve l'auto \n(lun=1, mar=2, mer=3, gio=4, ven=5, sab=6, dom=7, exit=0) : ";
+        cin>>n;
+        app[s]=n+3;
+        s++;
+        cout<<endl;
+    }
+    while (!sin.eof()){
+            sin>>w;
+            w.erase(remove(w.begin(),w.end(),x),w.end());
+            if(w==autom.categoria){
+            getline(sin, ap);
+            str[r]= w + "," + ap;
+            r++;
+            }
+    }
+    s--;
+    for (int f=0; f<s; f++)
+    {
+        cout<<"gg: "<<mat[2][f];
+        cout<< "sono dentro";
+        for (int o=app[f]; o<app[f]; o++)
+        {
+
+
+        if (mat[2][o]=="A")
+            str[o]=str[o+1];
+        else
+            if (mat[2][o]=="L"){
+                y++;
+            }
+        }
+    }
+    cout<<"s="<<s;
+    cout<<"y="<<y;
+    if (y==s)
+    cout<<"L'auto "<<str[0]<<" e' stata prenotata";
+
+    sin.close();
+    cout<<endl;
 }
 int main()
 {
